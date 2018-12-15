0e792145cc7544d01dc0e6e55dc5a50b2be24290
Ker Hong <ngukerhong@hotmail.com>
2018-12-15 18:39:43 +1000

    complete pig_latin.rb

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,15 @@
-#write your code here
+def translate string
+    translation = []
+    array = string.split(" ")
+    array.each do |word|
+        vowel_index = word.index(/[aeiou]/)
+        if (vowel_index == 0)
+		        translation << word + "ay"
+	      else
+            x = word.slice(0, vowel_index)
+		        y = word.slice(vowel_index, word.length)
+		        translation << y + x + "ay"
+ 	      end
+    end
+    return translation.join(" ")
+end
