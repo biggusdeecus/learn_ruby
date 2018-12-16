1dea31cebf8db0c814d30223a0a9787da3aa3e0b
biggusdeecus <43435407+biggusdeecus@users.noreply.github.com>
2018-12-16 19:18:39 +1000

    Update timer.rb

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -4,8 +4,20 @@ class Timer
   def initialize(seconds: 0)
     @seconds = seconds
   end
+  
   def time_string
+    seconds = @seconds % 60
+    minutes = @seconds / 60
+    hours = minutes / 60
     
+    return #{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}
+  end
   
-  
+  def padded(n)
+    if (n.to_s.length < 2)
+      return "0" + n.to_s
+    else
+      return n.to_s
+    end
+  end
 end
