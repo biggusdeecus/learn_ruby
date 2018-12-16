4299ff6d60b81901c0b54aaa96031073fc1b1e85
Ker Hong <ngukerhong@hotmail.com>
2018-12-16 17:57:44 +1000

    book.rb complete

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,24 @@
 class Book
-# write your code here
+	def title
+		return @title
+	end
+
+	def title=(value)
+		@title = convert(value)
+	end
+
+	def convert(string)
+		little_words = ["a", "an", "and", "the", "in", "of"]
+		array = string.split(" ")
+		array.map! do |word|
+			if !(little_words.include?(word))
+				word.capitalize
+			else
+				word = word
+			end
+		end
+		array = array.join(" ")
+		array[0] = array[0].capitalize
+		return array
+	end
 end
